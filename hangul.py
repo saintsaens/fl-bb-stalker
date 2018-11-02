3dcdefd1a25779cc868d517bb414776ab0780296
Faure <saintsaens@users.noreply.github.com>
2018-11-02 18:05:06 +0000

    Simplify Hangul character check.

--- hangul.py
+++ hangul.py
@@ -1,17 +1,9 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-def is_hangul_character(char):
-    """Check if character is in the Hangul Jamo block. MUST BE IN UNICODE."""
-
-    value = ord(char)
-    if 44032 <= value <= 55215:
-        return True
-    return False
-
-
 def is_hangul(string):
     """Check if there is a character in the Hangul syllables block. MUST BE IN UNICODE."""
 
     for i in string:
-        if is_hangul_character(i):
+        if 44032 <= ord(i) <= 55215:
             return True
     return False
--- test_hangul.py
+++ test_hangul.py
@@ -2,11 +2,6 @@
 import hangul
 
 
-def test_is_hangul_character_true():
-    assert hangul.is_hangul_character(u'한') is True
-    assert hangul.is_hangul_character(u'h') is False
-
-
 def test_is_hangul():
     assert hangul.is_hangul(u'한글이 줗아') is True
     assert hangul.is_hangul(u'한') is True
