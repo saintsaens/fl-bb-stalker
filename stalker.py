0d4c5846c9343df3f8c77fdc4b8095a6e3209414
Faure <saintsaens@users.noreply.github.com>
2018-11-02 20:20:09 +0000

    Send the damn mail.

--- stalker.py
+++ stalker.py
@@ -4,31 +4,46 @@ from bs4 import BeautifulSoup
 from hangul import is_hangul
 import conf
 import logger
+import mail
+import conf_mail
 
 
 def stalk():
     soup = BeautifulSoup(open(conf.HTML_FILE), 'html.parser')
 
-    # Get all threads.
+    # Get all threads in their HTML form.
     for thread in soup.find_all(class_=conf.THREAD_CLASS):
 
         # Extract the title of the thread.
-        title = thread.find(class_=conf.TITLE_CLASS)
-
-        # Extract the title of the thread and encode it in UTF-8.
-        text = title.get_text().encode('utf-8')
+        title = get_thread_title(thread)
 
         # Get the link and ID of the thread.
-        link = title.get('href')
-        thread_id = thread['id']
+        link = get_thread_link(thread)
+        thread_id = get_thread_id(thread)
 
         # Check if the thread title is in Korean (must be turned back to unicode).
-        if is_hangul(text.decode('utf-8')):
-            print("Thread in Korean found: " + text)
-            print("Link: " + link)
+        if is_hangul(title.decode('utf-8')):
+            print("Thread in Korean found: " + title)
             print ("ID: " + thread_id)
             if logger.is_in_log(thread_id, conf.LOG_FILE):
                 print (thread_id + " is in log.")
             else:
-                logger.write_in_log(text, thread_id, link, conf.LOG_FILE)
-                print (thread_id + " written in log.")
+                logger.write_in_log(title, thread_id, link, conf.LOG_FILE)
+                print thread_id + ": writing in log..."
+                print "Sending mail..."
+                mail_subject = mail.create_subject(title)
+                mail.send_email(conf_mail.SRC_EMAIL, conf_mail.SRC_PW, conf_mail.DST_EMAIL, mail_subject, link)
+
+
+def get_thread_title(thread):
+    html_title = thread.find(class_=conf.TITLE_CLASS)
+    return html_title.get_text().encode('utf-8')
+
+
+def get_thread_link(thread):
+    html_title = thread.find(class_=conf.TITLE_CLASS)
+    return html_title.get('href').encode('utf-8')
+
+
+def get_thread_id(thread):
+    return thread['id']
