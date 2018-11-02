17076de19f1a0c5c39b18ae668f768fbd952ea93
Faure <saintsaens@users.noreply.github.com>
2018-11-02 13:43:56 +0000

    Add verbose levels, and clean main file into stalker.

--- fl-bb-stalker.py
+++ fl-bb-stalker.py
@@ -1,36 +1,17 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from bs4 import BeautifulSoup
-from hangul import is_hangul
-import conf
-import logger
+import argparse
+import logging
+import stalker
+
+# ================================ PART 0: LOGGING THINGS =================================
+if __name__ == '__main__':
+    levels = ['info', 'debug', 'error', 'warning', 'critical']
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--level', dest='level', default='info',
+                        choices=['info', 'debug', 'error', 'warning', 'critical'])
+    args = parser.parse_args()
+    logging.basicConfig(level=getattr(logging, args.level.upper()))
 
 
-def fl_bb_stalker():
-    soup = BeautifulSoup(open(conf.HTML_FILE), 'html.parser')
-
-    # Get all threads.
-    for thread in soup.find_all(class_=conf.THREAD_CLASS):
-
-        # Extract the title of the thread.
-        title = thread.find(class_=conf.TITLE_CLASS)
-
-        # Extract the title of the thread and encode it in UTF-8.
-        text = title.get_text().encode('utf-8')
-
-        # Get the link and ID of the thread.
-        link = title.get('href')
-        thread_id = thread['id']
-
-        # Check if the thread title is in Korean (must be turned back to unicode).
-        if is_hangul(text.decode('utf-8')):
-            print("Thread in Korean found: " + text)
-            print("Link: " + link)
-            print ("ID: " + thread_id)
-            if logger.is_in_log(thread_id, conf.LOG_FILE):
-                print (thread_id + " is in log.")
-            else:
-                logger.write_in_log(thread_id)
-                print (thread_id + " written in log.")
-
-
-fl_bb_stalker()
+stalker.stalk()
