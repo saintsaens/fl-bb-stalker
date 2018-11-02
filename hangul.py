50a1ee423dee4abcd30d0ee7f97b99587e4524f6
Faure <saintsaens@users.noreply.github.com>
2018-11-02 15:22:02 +0000

    Add test cases.

--- hangul.py
+++ hangul.py
@@ -3,9 +3,9 @@ def is_hangul_character(char):
     """Check if character is in the Hangul Jamo block. MUST BE IN UNICODE."""
 
     value = ord(char)
-    # return 4352 <= value <= 4607
     if 44032 <= value <= 55215:
         return True
+    return False
 
 
 def is_hangul(string):
